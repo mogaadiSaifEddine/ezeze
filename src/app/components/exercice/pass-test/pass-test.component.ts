b018f10939f096d68f65d936310399e077fa1c72
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-02-11 19:39:24 +0100

    document formatting + removed console log

--- src/app/components/exercice/pass-test/pass-test.component.ts
+++ src/app/components/exercice/pass-test/pass-test.component.ts
@@ -27,7 +27,7 @@ export class PassTestComponent implements OnInit {
 
   loading = true;
 
-  constructor(private revisionService: RevisionService, private userService: UserService, private ss: SharedService) {}
+  constructor(private revisionService: RevisionService, private userService: UserService, private ss: SharedService) { }
 
   ngOnInit(): void {
     const userconnected = JSON.parse(localStorage.getItem('user_details'));
@@ -62,7 +62,6 @@ export class PassTestComponent implements OnInit {
       score: this.score,
       scoreToSend: this.answer ? 1 : 0,
       serie: this.evaluationContent.seriesType,
-
       user: this.UserId
     };
 
@@ -74,13 +73,9 @@ export class PassTestComponent implements OnInit {
   }
 
   canGoNext(event) {
-    console.log(this.answer);
-
     this.canGoNextQuestion = event;
   }
   checkAnswer() {
-    console.log(this.answer);
-
     if (this.answer !== null) {
       this.sendUserAnswerToTheServer();
       this.isAnswerChecked = true;
