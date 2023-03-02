1126a02ab687a1513900a6faada87c8d05968813
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-03-02 17:01:20 +0100

    console.log used for testing

--- src/app/components/enseignant/chapter-list/add-chapter/add-exercice/words-syllables-exercice/words-syllables-exercice.component.ts
+++ src/app/components/enseignant/chapter-list/add-chapter/add-exercice/words-syllables-exercice/words-syllables-exercice.component.ts
@@ -6,68 +6,68 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
   styleUrls: ['./words-syllables-exercice.component.scss']
 })
 export class WordsSyllablesExerciceInputComponent implements OnInit {
-  @Input() wordsSyllablesForm:FormGroup
-  @Output() wordsSyllablesFormChange:EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
+  @Input() wordsSyllablesForm: FormGroup
+  @Output() wordsSyllablesFormChange: EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
   @Input() defaultForm
-  
-  form:FormGroup
-  wordsArray:FormArray 
+
+  form: FormGroup
+  wordsArray: FormArray
   wordsForm: FormGroup;
   constructor(
-    private fb:FormBuilder
-  ){}
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
-    if(this.defaultForm){
+    if (this.defaultForm) {
       this.setDefaultForm();
-      this.removeWord(this.wordsForm.value.words.length-1)
-    }else{
+      this.removeWord(this.wordsForm.value.words.length - 1)
+    } else {
       this.wordsForm = this.fb.group({
         words: this.fb.array([
           this.fb.group({
-            word:this.fb.array([
+            word: this.fb.array([
               this.fb.group({
-                syllable:[null]
+                syllable: [null]
               })
             ]),
           })
-        ]) ,
+        ]),
       });
     }
 
-    this.wordsForm.valueChanges.subscribe(res=>{
+    this.wordsForm.valueChanges.subscribe(res => {
       this.wordsSyllablesFormChange.emit(this.wordsForm);
     })
   }
-  setDefaultForm(){
-    
+  setDefaultForm() {
+
     this.wordsForm = this.fb.group({
       words: this.fb.array([
         this.fb.group({
-          word:this.fb.array([]),
+          word: this.fb.array([]),
         })
       ]),
     });
 
-    this.defaultForm.blocks.forEach((word,index) => {
+    this.defaultForm.blocks.forEach((word, index) => {
       this.addWords(false)
-      word.correctValue.split('/').filter(syllable=>syllable!='').forEach(syllable=>{    
-        this.addSyllable(index,syllable)
+      word.correctValue.split('/').filter(syllable => syllable != '').forEach(syllable => {
+        this.addSyllable(index, syllable)
       })
     });
   }
-  newWord(withSyllable?:boolean): FormGroup {
-    if(withSyllable){
+  newWord(withSyllable?: boolean): FormGroup {
+    if (withSyllable) {
       return this.fb.group({
-        word:this.fb.array([
+        word: this.fb.array([
           this.fb.group({
-            syllable:[null]
+            syllable: [null]
           })
         ]),
       })
-    }else{
+    } else {
       return this.fb.group({
-        word:this.fb.array([
+        word: this.fb.array([
         ]),
       })
     }
@@ -75,25 +75,27 @@ export class WordsSyllablesExerciceInputComponent implements OnInit {
   addWords(withSyllable?) {
     this.words.push(this.newWord(withSyllable));
   }
-  addSyllable(index:number,syllable?:string){
+  addSyllable(index: number, syllable?: string) {
     (this.words.controls[index].get('word') as FormArray).push(this.fb.group({
-      syllable:[syllable?syllable:null]
+      syllable: [syllable ? syllable : null]
     }))
   }
-  removeSyllable(i,j){
+  removeSyllable(i, j) {
     (this.words.controls[i].get('word') as FormArray).removeAt(j)
   }
-  getSyllableArray(index){
+  getSyllableArray(index) {
     return this.words.controls[index].get('word') as FormArray
   }
-  removeWord(i:number) {
+  removeWord(i: number) {
     this.words.removeAt(i);
   }
-  get words() : FormArray {
+  onSubmit() {
+    console.log(this.wordsForm.value);
+  }
+  get words(): FormArray {
     return this.wordsForm.get("words") as FormArray
   }
   addWord() {
     this.words.push(new FormControl(''));
   }
-
 }
