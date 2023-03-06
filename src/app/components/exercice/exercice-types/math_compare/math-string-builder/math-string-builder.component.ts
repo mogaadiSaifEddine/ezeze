e5abb8f2453ace4e1e039cb85e4b719458645ee4
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-03-06 14:49:31 +0100

    initial setiup - add k-p : type-blocks - 2 compoennts (display, builder)

---
+++ src/app/components/exercice/exercice-types/math_compare/math-string-builder/math-string-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ines-math-string-builder',
+  templateUrl: './math-string-builder.component.html',
+  styleUrls: ['./math-string-builder.component.scss']
+})
+export class MathStringBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/exercice/exercice-types/math_compare/math-string-display/math-string-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ines-math-string-display',
+  templateUrl: './math-string-display.component.html',
+  styleUrls: ['./math-string-display.component.scss']
+})
+export class MathStringDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
