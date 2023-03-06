e7c8251e49366e5bf8c90533304104c79cda78ec
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-03-06 15:18:13 +0100

    done with the builder

--- src/app/components/exercice/exercice-types/math_compare/math-string-builder/math-string-builder.component.ts
+++ src/app/components/exercice/exercice-types/math_compare/math-string-builder/math-string-builder.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { ExerciceBlock } from 'src/app/model/ExerciceBlock';
 import { ExerciceBlockTypes } from 'src/app/model/ExerciceBlockTypes';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ines-math-string-builder',
@@ -14,29 +15,39 @@ export class MathStringBuilderComponent implements OnInit {
   teachersParagraph: any;
   correctParagraph: any;
   ParagraphBlock: ExerciceBlock;
+  mathForm !: FormGroup;
 
-  constructor(private dialogRef: MatDialogRef<MathStringBuilderComponent>) { }
+  constructor(
+    private dialogRef: MatDialogRef<MathStringBuilderComponent>,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.mathForm = this.fb.group({
+      numbers: [null, Validators.required],
+      correctString: [null, Validators.required]
+    })
   }
 
 
   save() {
-    this.ParagraphBlock = {
-      exerciceBlockId: null,
-      exerciceId: null,
-      label: null,
-      correctValue: null,
-      isAdmissable: null,
-      placeholder: null,
-      value: null,
-      blockOrder: null,
-      files: null,
-      exerciceBlockType: ExerciceBlockTypes.MATH_STRING,
-      blockParams: null
+    if (this.mathForm.valid) {
+      console.log();
+      this.ParagraphBlock = {
+        exerciceBlockId: null,
+        exerciceId: null,
+        label: null,
+        correctValue: this.mathForm.value.correctString,
+        isAdmissable: null,
+        placeholder: null,
+        value: this.mathForm.value.numbers,
+        blockOrder: null,
+        files: null,
+        exerciceBlockType: ExerciceBlockTypes.MATH_STRING,
+        blockParams: null
+      }
+
+      this.dialogRef.close(this.ParagraphBlock);
     }
-
-    this.dialogRef.close(this.ParagraphBlock);
   }
-
 }
