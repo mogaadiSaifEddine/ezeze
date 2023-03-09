4e78a1692e3227fcf355b795b2a7d3d9b430cdb8
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-03-09 14:57:40 +0100

    DONE

--- src/app/components/exercice/exercice-types/color-shapes-and-images/csi-display/csi-display.component.ts
+++ src/app/components/exercice/exercice-types/color-shapes-and-images/csi-display/csi-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, Renderer2 } from '@angular/core';
 import { Exercice } from 'src/app/model/Exercice';
 import { ExerciceBlockTypes } from 'src/app/model/ExerciceBlockTypes';
 import * as _ from 'lodash';
@@ -15,27 +15,57 @@ export class CsiDisplayComponent implements OnInit {
   @Output() answerChange = new EventEmitter<boolean>();
   @Output() canGoNext = new EventEmitter<boolean>();
 
-  constructor() { }
+  CORRECT_ANSWER_COMBINATION: boolean[] = [];
+  USER_ANSWER_COMBINATION: boolean[] = [];
+  FINAL_BOOLEAN: boolean = false;
+
+  constructor(private render: Renderer2) { }
 
   ngOnInit(): void {
     this.initExercice();
   }
 
   private initExercice() {
-    this.exercice.blocks.forEach((block: any) => {
+    // init mirror arrays to use in validation
+    this.CORRECT_ANSWER_COMBINATION = new Array(this.exercice.blocks.length).fill('*');
+    this.USER_ANSWER_COMBINATION = new Array(this.exercice.blocks.length).fill('*');
+
+    this.exercice.blocks.forEach((block: any, index: number) => {
       // SENTENCE BLOCKS  
-      if (block.exerciceBlockType === ExerciceBlockTypes.ANSWER_TO_STROKE) {
+      if (block.exerciceBlockType === ExerciceBlockTypes.SHAPES_IMAGES_TO_COLOR) {
         block.blockParams = JSON.parse(block.blockParams);
-        // CUSTOM LOGIC
+        this.CORRECT_ANSWER_COMBINATION[index] = block.blockParams.image_is_colored;
       }
     });
+  }
+
+  toggleColor(event: any, colorToUse: string, index: number) {
+    if (event.target.className.includes('selected')) {
+      this.render.removeClass(event.target, "selected");
+      this.render.setStyle(event.target, 'background-color', '#fff');
+      this.kepTrackOfScore(false, index);
+    } else {
+      this.kepTrackOfScore(true, index);
+      this.render.setStyle(event.target, 'background-color', colorToUse);
+      this.render.addClass(event.target, "selected");
+    }
+
+  }
+
+  kepTrackOfScore(state: boolean, index: number) {
+    this.USER_ANSWER_COMBINATION[index] = state;
+
+    this.FINAL_BOOLEAN = _.isEqual(this.USER_ANSWER_COMBINATION, this.CORRECT_ANSWER_COMBINATION);
+
+    this.answerChange.emit(this.FINAL_BOOLEAN);
+    this.canGoNext.emit(true);
   }
 
 
 
   ngOnDestroy() {
     this.exercice.blocks.forEach((block: any) => {
-      if (block.exerciceBlockType === ExerciceBlockTypes.ANSWER_TO_STROKE) {
+      if (block.exerciceBlockType === ExerciceBlockTypes.SHAPES_IMAGES_TO_COLOR) {
         block.blockParams = JSON.stringify(block.blockParams);
       }
     });
