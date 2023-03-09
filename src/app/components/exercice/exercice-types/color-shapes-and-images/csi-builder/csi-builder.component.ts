ce4404eff5bffa53c87b815019aa023bc2b33ae4
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-03-09 11:00:44 +0100

    boilerplate | builder component

--- src/app/components/exercice/exercice-types/color-shapes-and-images/csi-builder/csi-builder.component.ts
+++ src/app/components/exercice/exercice-types/color-shapes-and-images/csi-builder/csi-builder.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { ExerciceBlock } from 'src/app/model/ExerciceBlock';
+import { ExerciceBlockTypes } from 'src/app/model/ExerciceBlockTypes';
 
 @Component({
   selector: 'ines-csi-builder',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CsiBuilderComponent implements OnInit {
 
-  constructor() { }
+  finalBlock: ExerciceBlock;
+
+
+  constructor(
+    private dialogRef: MatDialogRef<CsiBuilderComponent>
+  ) { }
 
   ngOnInit(): void {
+    // init form if there is any
   }
 
+
+  saveBlock() {
+    this.finalBlock = {
+      exerciceBlockId: null,
+      exerciceId: null,
+      label: null,
+      correctValue: null,
+      isAdmissable: null,
+      placeholder: null,
+      value: null,
+      blockOrder: null,
+      files: null,
+      exerciceBlockType: ExerciceBlockTypes.SHAPES_IMAGES_TO_COLOR,
+      blockParams: JSON.stringify({ params: 'insert oibject or set to null' })
+    }
+
+    this.dialogRef.close(this.finalBlock);
+  }
 }
