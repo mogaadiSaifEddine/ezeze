b1c510a7b34227d10c5256e7b127023e8c4cff55
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-03-09 10:51:43 +0100

    boilerplate display

--- src/app/components/exercice/exercice-types/color-shapes-and-images/csi-display/csi-display.component.ts
+++ src/app/components/exercice/exercice-types/color-shapes-and-images/csi-display/csi-display.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Exercice } from 'src/app/model/Exercice';
+import { ExerciceBlockTypes } from 'src/app/model/ExerciceBlockTypes';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'ines-csi-display',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CsiDisplayComponent implements OnInit {
 
+  @Input() exercice: Exercice;
+  @Input() answer: boolean;
+  @Output() answerChange = new EventEmitter<boolean>();
+  @Output() canGoNext = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initExercice();
+  }
+
+  private initExercice() {
+    this.exercice.blocks.forEach((block: any) => {
+      // SENTENCE BLOCKS  
+      if (block.exerciceBlockType === ExerciceBlockTypes.ANSWER_TO_STROKE) {
+        block.blockParams = JSON.parse(block.blockParams);
+        // CUSTOM LOGIC
+      }
+    });
+  }
+
+
+
+  ngOnDestroy() {
+    this.exercice.blocks.forEach((block: any) => {
+      if (block.exerciceBlockType === ExerciceBlockTypes.ANSWER_TO_STROKE) {
+        block.blockParams = JSON.stringify(block.blockParams);
+      }
+    });
   }
 
 }
