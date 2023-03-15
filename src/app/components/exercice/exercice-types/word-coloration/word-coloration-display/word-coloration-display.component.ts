a6be583768406e2a7688020ea0b09711fa956fc4
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-03-15 08:41:59 +0100

    word-coloration display component

--- src/app/components/exercice/exercice-types/word-coloration/word-coloration-display/word-coloration-display.component.ts
+++ src/app/components/exercice/exercice-types/word-coloration/word-coloration-display/word-coloration-display.component.ts
@@ -1,20 +1,28 @@
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
-import { Exercice } from 'src/app/model/Exercice';
+import { AnswerFeedback, Exercice } from 'src/app/model/Exercice';
 import { ExerciceBlockTypes } from 'src/app/model/ExerciceBlockTypes';
 import * as _ from 'lodash';
+import { ExerciceBlock } from 'src/app/model/ExerciceBlock';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 @Component({
   selector: 'ines-word-coloration-display',
   templateUrl: './word-coloration-display.component.html',
   styleUrls: ['./word-coloration-display.component.scss']
 })
 export class WordColorationDisplayComponent implements OnInit, OnDestroy {
-
   @Input() exercice: Exercice;
   @Input() answer: boolean;
   @Output() answerChange = new EventEmitter<boolean>();
   @Output() canGoNext = new EventEmitter<boolean>();
+  cursorColor = '';
+  width = 10;
+  color: string;
+  alpha = 1;
 
-  constructor() { }
+  get cursor(): SafeStyle {
+    return this.sanitizer.bypassSecurityTrustStyle(this.makeCursor());
+  }
+  constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     this.initExercice();
@@ -22,20 +30,57 @@ export class WordColorationDisplayComponent implements OnInit, OnDestroy {
 
   private initExercice() {
     this.exercice.blocks.forEach((block: any) => {
-      // SENTENCE BLOCKS  
-      if (block.exerciceBlockType === ExerciceBlockTypes.ANSWER_TO_STROKE) {
-        block.blockParams = JSON.parse(block.blockParams);
-        // CUSTOM LOGIC
+      if (block.exerciceBlockType === ExerciceBlockTypes.COLOR_PARAMS) {
+        block.blockParams = <Array<any>>JSON.parse(block.blockParams);
+        block.blockFinal = <Array<any>>block.blockParams;
+
+        block.blockFinal.forEach((element) => {
+          element.crrentColor = '#000000';
+        });
       }
-    })
+    });
+  }
+  selectColor(color: string) {
+    this.cursorColor = color;
+  }
+  colorText(i: number, j: number) {
+    this.exercice.blocks[i].blockFinal[j].crrentColor = this.cursorColor;
+    this.canGoNext.emit(true);
+    this.verfiAnswer();
+  }
+  verfiAnswer() {
+    let answer = true;
+
+    for (let block of this.exercice.blocks) {
+      if (block.blockFinal.some((el) => el.color !== el.crrentColor)) {
+        answer = false;
+        break;
+      }
+    }
+
+    this.answerChange.emit(answer);
+  }
+  makeCursor() {
+    const cursor = document.createElement('canvas'),
+      ctx = cursor.getContext('2d');
+
+    cursor.width = this.width;
+    cursor.height = this.width;
+    ctx.strokeStyle = this.cursorColor ?? 'black';
+    ctx.fillStyle = this.cursorColor ?? 'black';
+    ctx.globalAlpha = this.alpha;
+    ctx.beginPath();
+    ctx.arc(this.width / 2, this.width / 2, this.width / 2, 0, 2 * Math.PI);
+    ctx.fill();
+
+    return `url(${cursor.toDataURL()}) ${this.width / 2} ${this.width / 2}, auto`;
   }
 
   ngOnDestroy() {
     this.exercice.blocks.forEach((block: any) => {
-      if (block.exerciceBlockType === ExerciceBlockTypes.ANSWER_TO_STROKE) {
+      if (block.exerciceBlockType === ExerciceBlockTypes.COLOR_PARAMS) {
         block.blockParams = JSON.stringify(block.blockParams);
       }
-    })
+    });
   }
-
 }
