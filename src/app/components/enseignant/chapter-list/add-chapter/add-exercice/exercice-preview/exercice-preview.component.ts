a9322db84fe16779d96644340611772046b7c925
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-03-21 08:53:03 +0100

    create a shared fonction  replaceBreaksInQuestion && puted in the exercice preview

--- src/app/components/enseignant/chapter-list/add-chapter/add-exercice/exercice-preview/exercice-preview.component.ts
+++ src/app/components/enseignant/chapter-list/add-chapter/add-exercice/exercice-preview/exercice-preview.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Exercice } from 'src/app/model/Exercice';
 import { Exercise_Types } from 'src/app/model/Exercice_type';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-exercice-preview',
@@ -9,11 +10,10 @@ import { Exercise_Types } from 'src/app/model/Exercice_type';
   styleUrls: ['./exercice-preview.component.scss']
 })
 export class ExercicePreviewComponent implements OnInit {
-
   readonly TYPES = Exercise_Types;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {currentExercise:Exercice}){}
-  ngOnInit(){
-
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { currentExercise: Exercice }, private sharedService: SharedService) {}
+  ngOnInit() {
+    this.data.currentExercise.name = this.sharedService.replaceBreaksInQuestion(this.data.currentExercise.name);
   }
 }
